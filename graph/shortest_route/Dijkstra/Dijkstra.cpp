868f80d0ab4795e0f8f47602552b5a2624b03d8b
Acan <xie.zeyu20@gmail.com>
2023-11-08 14:17:13 +0800

    .

--- graph/shortest_route/Dijkstra/Dijkstra.cpp
+++ graph/shortest_route/Dijkstra/Dijkstra.cpp
@@ -7,7 +7,10 @@ using namespace std;
 
 int n, m, s;
 int tmp1, tmp2, tmp3;
+int tmpChoice;
+bool isEnd = false;
 int dist[105][105];
+bool isBooked[105];
 
 template <typename T>
 T _min(T a, T b)
@@ -34,6 +37,10 @@ int main()
                 dist[i][j] = 0;
     }
 
+    // Initialize the booking info
+    memset(isBooked, false, sizeof(isBooked));
+    isBooked[s] = true;
+
     // Input lengths of the m directed edges
     for (int i = 0; i < m; i++)
     {
@@ -41,8 +48,46 @@ int main()
         dist[tmp1][tmp2] = tmp3;
     }
 
-    while (true)
+    while (!isEnd)
     {
+        // Initalize the isEnd and tmpDist
+        isEnd = true;
+        tmpChoice = -1;
+
+        // Find the closeset unbooked node to s
+        for (int i = 0; i < n; i++)
+        {
+
+            // If node i has been booked
+            if (isBooked[i])
+                continue;
+
+            // If node i is not reachable from node s
+            else if (dist[s][i] >= maxDist)
+                continue;
+
+            // If node i has not been booked, but there is already another closer node
+            else if (tmpChoice >= 0 && dist[s][tmpChoice] <= dist[s][i])
+                continue;
+
+            // Set the tmpChoice and the isEnd
+            tmpChoice = i;
+            isEnd = false;
+        }
+
+        // If no node needs consideration
+        if (isEnd)
+            break;
+
+        // Book the node tmpChoice
+        isBooked[tmpChoice] = true;
+
+        // Renew the distances of unbooked nodes
+        for (int i = 0; i < n; i++)
+        {
+            if (!isBooked[i])
+                dist[s][i] = _min(dist[s][i], dist[s][tmpChoice] + dist[tmpChoice][i]);
+        }
     }
 
     // Print the distances from node s to all nodes (Single source minimal route)
