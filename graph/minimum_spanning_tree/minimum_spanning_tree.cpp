11993fbb7bc4e4b920720dffea12a2cc12a495ae
Acan <xie.zeyu20@gmail.com>
2023-11-09 14:46:46 +0800

    .

--- graph/minimum_spanning_tree/minimum_spanning_tree.cpp
+++ graph/minimum_spanning_tree/minimum_spanning_tree.cpp
@@ -1,8 +1,34 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+
+// Import union find data structure
 #include "union_find.hpp"
-#include<cstdio>
-#include<iostream>
+
 using namespace std;
 
-int main() {
+class Edge
+{
+public:
+    int x;
+    int y;
+    int dist;
+
+    // Constructive function
+    Edge(int x, int y, int dist) : x(x), y(y), dist(dist) {}
+
+    // Operator overloading
+    bool operator<(const Edge &other) const
+    {
+        return dist < other.dist;
+    }
+};
+
+int n, m;
+int tmp1, tmp2, tmp3;
+Edge edge[1005];
+
+int main()
+{
     
 }
