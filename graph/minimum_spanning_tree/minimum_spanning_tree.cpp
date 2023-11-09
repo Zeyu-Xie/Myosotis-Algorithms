9ec90e184a464eddcb79243daa98145b2b3944c0
Acan <xie.zeyu20@gmail.com>
2023-11-09 14:55:13 +0800

    .

--- graph/minimum_spanning_tree/minimum_spanning_tree.cpp
+++ graph/minimum_spanning_tree/minimum_spanning_tree.cpp
@@ -14,7 +14,8 @@ public:
     int y;
     int dist;
 
-    // Constructive function
+    // Constructive functions
+    Edge() : x(0), y(0), dist(0) {}
     Edge(int x, int y, int dist) : x(x), y(y), dist(dist) {}
 
     // Operator overloading
@@ -26,9 +27,41 @@ public:
 
 int n, m;
 int tmp1, tmp2, tmp3;
+int num = 0;
 Edge edge[1005];
 
 int main()
 {
-    
+    freopen("./minimum_spanning_tree.in", "r", stdin);
+    cin >> n >> m;
+
+    UnionFind uf(m);
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> tmp1 >> tmp2 >> tmp3;
+        edge[i] = Edge(tmp1, tmp2, tmp3);
+    }
+
+    sort(edge, edge + m);
+
+    for (int i = 0; i < m; i++)
+    {
+        int x = edge[i].x;
+        int y = edge[i].y;
+        int dist = edge[i].dist;
+
+        // If x and y have already been connected
+        if (uf.areConnected(x, y))
+            continue;
+
+        // If the number of chosen edges is enough
+        if (num >= n - 1)
+            break;
+
+        cout << "(" << x << ", " << y << ") " << dist << " ";
+        cout << "\n";
+    }
+
+    return 0;
 }
